4d8394eb35f9719fdd6e9137770d40fae13ad0c2
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2023-03-08 21:30:14 +0600

    upd

--- lab6/src/app/app.module.ts
+++ lab6/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { AlbumPhotosComponent } from './album-photos/album-photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent,
+    AlbumsComponent,
+    AlbumDetailComponent,
+    AlbumPhotosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ lab6/src/app/models.ts
@@ -0,0 +1,14 @@
+export interface Album {
+  id: number;
+  title: string;
+
+}
+
+
+export interface Photo {
+  album_id: number,
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}

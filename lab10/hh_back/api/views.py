1518e19edcd153842262568b2af20e41740ceb21
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2023-04-18 23:42:52 +0600

    upd

--- lab10/hh_back/api/views.py
+++ lab10/hh_back/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import View
 
 from .models import Company, Vacancy
+from .serializers import CompanySerializer, VacancySerializer
 
 
 # from lab9.hh_back.api.models import Vacancy
@@ -10,11 +11,12 @@ from .models import Company, Vacancy
 
 # Create your views here.
 
+
 class CompanyListView(View):
     def get(self, request):
         companies = Company.objects.all()
-        companies_json = [p.to_json() for p in companies]
-        return JsonResponse(companies_json, safe=False)
+        serializer = CompanySerializer(companies, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
 
 class CompanyDetailView(View):
@@ -23,21 +25,21 @@ class CompanyDetailView(View):
             company = Company.objects.get(id=id)
         except Company.DoesNotExist:
             return JsonResponse({'error': 'Company not found'}, status=404)
-        return JsonResponse(company.to_json(), safe=False)
+        return JsonResponse(CompanySerializer(company).data, safe=False)
 
 
 class CompanyVacanciesView(View):
     def get(self, request, id):
         company_name = Company.objects.get(id=id)
         vacancies = Vacancy.objects.filter(company=company_name)
-        vacancies_json = [p.to_json() for p in vacancies]
-        return JsonResponse(vacancies_json, safe=False)
+        serializer = VacancySerializer(vacancies, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
 
 def vacancies_list(request):
     vacancies = Vacancy.objects.all()
-    vacancies_json = [p.to_json() for p in vacancies]
-    return JsonResponse(vacancies_json, safe=False)
+    serializer = VacancySerializer(vacancies, many=True)
+    return JsonResponse(serializer.data, safe=False)
 
 
 def get_vacancy_by_id(request, id):
@@ -45,10 +47,10 @@ def get_vacancy_by_id(request, id):
         vacancy = Vacancy.objects.get(id=id)
     except Vacancy.DoesNotExist:
         return JsonResponse({'error': 'Vacancy not found'}, status=404)
-    return JsonResponse(vacancy.to_json(), safe=False)
+    return JsonResponse(VacancySerializer(vacancy).data, safe=False)
 
 
 def top_vacancies(request):
     vacancies = Vacancy.objects.order_by('-salary')[:10]
-    vacancies_json = [p.to_json() for p in vacancies]
-    return JsonResponse(vacancies_json, safe=False)
+    serializer = VacancySerializer(vacancies, many=True)
+    return JsonResponse(serializer.data, safe=False)

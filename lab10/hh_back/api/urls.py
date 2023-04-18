f5a1bc70e2e0da94c21c77dcdbebf1c40a70a763
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2023-04-18 23:10:29 +0600

    upd

--- lab10/hh_back/api/urls.py
+++ lab10/hh_back/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('companies/', company_list),
-    path('companies/<int:id>/', get_company_by_id),
-    path('companies/<int:id>/vacancies/', get_vacancies_by_company_id),
+    path('companies/', CompanyListView.as_view(), name="listview"),
+    path('companies/<int:id>/', CompanyDetailView.as_view(), name="detailview"),
+    path('companies/<int:id>/vacancies/', CompanyVacanciesView.as_view(), name="vacancies"),
     path('vacancies/', vacancies_list),
     path('vacancies/<int:id>/', get_vacancy_by_id),
     path('vacancies/top_ten/', top_vacancies)
--- lab10/hh_back/api/views.py
+++ lab10/hh_back/api/views.py
@@ -1,38 +1,43 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
 
 from .models import Company, Vacancy
+
 
 # from lab9.hh_back.api.models import Vacancy
 
 
 # Create your views here.
 
-def company_list(request):
-    companies = Company.objects.all()
-    companies_json = [p.to_json() for p in companies]
-    return JsonResponse(companies_json, safe = False)
+class CompanyListView(View):
+    def get(self, request):
+        companies = Company.objects.all()
+        companies_json = [p.to_json() for p in companies]
+        return JsonResponse(companies_json, safe=False)
 
 
-def get_company_by_id(request, id):
-    try:
-        company = Company.objects.get(id=id)
-    except Company.DoesNotExist:
-        return JsonResponse({'error': 'Company not found'}, status=404)
-    return JsonResponse(company.to_json(), safe=False)
+class CompanyDetailView(View):
+    def get(self, request, id):
+        try:
+            company = Company.objects.get(id=id)
+        except Company.DoesNotExist:
+            return JsonResponse({'error': 'Company not found'}, status=404)
+        return JsonResponse(company.to_json(), safe=False)
 
 
-def get_vacancies_by_company_id(request, id):
-    company_name = Company.objects.get(id=id)
-    vacancies = Vacancy.objects.filter(company=company_name)
-    vacancies_json = [p.to_json() for p in vacancies]
-    return JsonResponse(vacancies_json, safe=False)
+class CompanyVacanciesView(View):
+    def get(self, request, id):
+        company_name = Company.objects.get(id=id)
+        vacancies = Vacancy.objects.filter(company=company_name)
+        vacancies_json = [p.to_json() for p in vacancies]
+        return JsonResponse(vacancies_json, safe=False)
 
 
 def vacancies_list(request):
     vacancies = Vacancy.objects.all()
     vacancies_json = [p.to_json() for p in vacancies]
-    return JsonResponse(vacancies_json,safe=False)
+    return JsonResponse(vacancies_json, safe=False)
 
 
 def get_vacancy_by_id(request, id):
@@ -40,11 +45,10 @@ def get_vacancy_by_id(request, id):
         vacancy = Vacancy.objects.get(id=id)
     except Vacancy.DoesNotExist:
         return JsonResponse({'error': 'Vacancy not found'}, status=404)
-    return JsonResponse(vacancy.to_json(),safe=False)
+    return JsonResponse(vacancy.to_json(), safe=False)
 
 
 def top_vacancies(request):
     vacancies = Vacancy.objects.order_by('-salary')[:10]
     vacancies_json = [p.to_json() for p in vacancies]
     return JsonResponse(vacancies_json, safe=False)
-
